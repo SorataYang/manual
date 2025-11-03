8ebd02914e539d26730ea252bbf301408e0a86c6
Sorata <194564851+SorataYang@users.noreply.github.com>
2025-11-03 11:12:47 +0800

    Update GitHub user name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Sorata', // Usually your GitHub org/user name.
+  organizationName: 'SorataYang', // Usually your GitHub org/user name.
   projectName: 'manual', // Usually your repo name.
 
   onBrokenLinks: 'throw',
